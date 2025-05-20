a66693cf6a8c58b24deb8617bc2b5f0bf382ca19
Dani Resende <danielresende2000@gmail.com>
2025-05-19 22:09:47 -0300

    feat: implementing knapsack problem

---
+++ TP2/knapsack_problem/knapsack_problem.py
@@ -0,0 +1,84 @@
+import numpy as np
+import numpy.random as rnd
+import csv
+
+# INITIALIZING VARIABLES
+obj_prices = []
+obj_weights = []
+with open('mochila.txt', 'r') as fin:
+    for i, row in enumerate(csv.reader(fin)):
+        if i == 1:
+            n_obj = int(row[0])
+        if i == 2:
+            max_load = int(row[0])
+        if i > 3:
+            p, w = str.split(row.pop(),' ')
+            obj_prices.append(int(p))
+            obj_weights.append(int(w))
+
+genome_size = n_obj
+pop_size = 100
+gen_limit = 2000
+ideal_fitness = sum(obj_prices)
+mutation_rate = 0.05
+
+# FITNESS FUNCTION
+def fit_backpack(gene, weights, prices):
+    if(np.dot(gene, weights) <= max_load):
+        return np.dot(gene, prices)
+    return 0
+
+
+
+# EXECUTION
+population = rnd.randint(low=0, high=2, size=(pop_size, genome_size))
+best_fit_per_gen = np.zeros(gen_limit)
+
+for gen_number in range(0, gen_limit):
+
+    # fitness scoring
+    fit_scores = []
+    for i in range(0, pop_size):
+        fit_scores.append(fit_backpack(population[i], obj_weights, obj_prices))
+    
+    # best scores
+    best_fit_per_gen[gen_number] = max(fit_scores)
+    best_fit_idx = np.argmax(fit_scores)
+    best_genome = population[best_fit_idx]
+
+    # end criteria
+    if best_fit_per_gen[gen_number] >= ideal_fitness:
+        break
+
+    # parent selection
+    parents = np.zeros(shape=(pop_size, genome_size))
+
+    for i in range(0, pop_size):
+        idx_first_parent = rnd.randint(pop_size)
+        idx_second_parent = rnd.randint(pop_size)
+        if fit_scores[idx_first_parent] > fit_scores[idx_second_parent]:
+            parents[i] = population[idx_first_parent]
+        else:
+            parents[i] = population[idx_second_parent]
+
+    # crossover
+    offspring = np.zeros(shape=(pop_size, genome_size))
+    for i in range(0, pop_size):
+        crossover_point = rnd.randint(genome_size - 1)
+        offspring[i][0:crossover_point] = parents[i][0:crossover_point]
+        offspring[i][crossover_point+1:genome_size-1] = parents[i][crossover_point+1:genome_size-1]
+
+    # mutation
+    for i in range(0, pop_size):
+        for j in range(0, genome_size):
+            if rnd.random() < mutation_rate:
+                offspring[i][j] = int(not(offspring[i][j]))
+
+    # new population
+    population = offspring
+    population[0] = best_genome
+
+print("Melhor genoma: \n", best_genome)
+print("Valor: \n", np.dot(best_genome, obj_prices), " do total ", ideal_fitness)
+print("Peso: \n", np.dot(best_genome, obj_weights), " do máximo ", max_load)
+print("Geração do melhor resultado: \n", np.argmax(best_fit_per_gen))
